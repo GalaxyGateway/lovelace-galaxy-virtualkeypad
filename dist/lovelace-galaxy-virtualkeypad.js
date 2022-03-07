9f5077c47a1336a1c7a354fb3acc98009d5f5d0d
GalaxyGateway <aequitas@Tjeerds-MBP-2.localdomain>
2022-03-07 08:49:37 +0200

    Update lovelace-galaxy-virtualkeypad.js

--- dist/lovelace-galaxy-virtualkeypad.js
+++ dist/lovelace-galaxy-virtualkeypad.js
@@ -13,6 +13,19 @@ window.customCards.push({
   documentationURL: "https://github.com/GalaxyGateway/lovelace-galaxy-virtualkeypad",
 });
 
+const fireEvent = (node, type, detail, options) => {
+  options = options || {};
+  detail = detail === null || detail === undefined ? {} : detail;
+  const event = new Event(type, {
+    bubbles: options.bubbles === undefined ? true : options.bubbles,
+    cancelable: Boolean(options.cancelable),
+    composed: options.composed === undefined ? true : options.composed,
+  });
+  event.detail = detail;
+  node.dispatchEvent(event);
+  return event;
+};
+
 function hasConfigOrEntityChanged(element, changedProps) {
   if (changedProps.has("_config")) {
     return true;
