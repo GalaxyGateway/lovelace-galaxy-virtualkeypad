bb5778c46740f5a73f808ee81b7cc72da73728bc
GalaxyGateway <80357370+GalaxyGateway@users.noreply.github.com>
2026-03-01 12:34:40 +0200

    Update lovelace-galaxy-virtualkeypad.js
    Updated documentation links and corrected files location for audio files

--- dist/lovelace-galaxy-virtualkeypad.js
+++ dist/lovelace-galaxy-virtualkeypad.js
@@ -1,5 +1,5 @@
 console.info(
-  "%c  lovelace-galaxy-virtualkeypad  \n%c Version 0.0.5 ",
+  "%c  lovelace-galaxy-virtualkeypad  \n%c Version 0.0.6 ",
   "color: orange; font-weight: bold; background: black",
   "color: white; font-weight: bold; background: dimgray"
 );
@@ -16,7 +16,7 @@ window.customCards.push({
   name: "Galaxy Keypad",
   description: "A virtual keypad for Honeywell Galaxy.",
   preview: true,
-  documentationURL: "https://github.com/GalaxyGateway/lovelace-galaxy-virtualkeypad",
+  documentationURL: "https://github.com/GalaxyGateway/lovelace-galaxy-virtualkeypad_new",
 });
 
 const fireEvent = (node, type, detail, options) => {
@@ -85,7 +85,7 @@ class AlarmKeypad extends LitElement {
   // recreated during updates, preventing playback interruption.
   firstUpdated() {
     if (this._config.audio !== false) {
-      const base = "/local/community/lovelace-galaxy-virtualkeypad/";
+      const base = "/hacsfiles/lovelace-galaxy-virtualkeypad_new/";
       const audioContainer = document.createElement("div");
       audioContainer.innerHTML = `
         <audio id="exitsound1" loop><source src="${base}beep.mp3" type="audio/mpeg"></audio>
